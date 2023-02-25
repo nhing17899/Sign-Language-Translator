6a7c89d0b5a264f33e3ae9cd69859224290755c4
nguyenleviet <85124005+nguyenlevid@users.noreply.github.com>
2023-02-25 10:51:07 -0600

    implementing quiz logic

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const itemRouter = require("./routes/itemRoutes");
 const textInputRouter = require("./routes/textInputRoutes");
+const quizRouter = require("./routes/quizRoutes");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/v1/items", itemRouter);
 app.use("/api/v1/translation", textInputRouter);
+app.use("/api/v1/quiz", quizRouter);
 
 module.exports = app;
