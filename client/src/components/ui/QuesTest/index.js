53e1df5510c2b415205e0bee7f72dbc3565f6db6
Nhi Nguyen <nhing17899@gmail.com>
2023-02-26 05:14:59 -0500

    Create Test

--- client/src/components/ui/QuesTest/index.js
+++ client/src/components/ui/QuesTest/index.js
@@ -2,17 +2,36 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "./QuesTest.css";
 
-const axios = require("axios").default;
-
-export const ImgToText = (props) => {
-    const titleTest = "Topic Test : Animals";
-
+const ImgToText = (props) => {
     const { quizType, question, answer } = props; 
-    // quizType: "image to text"
 
     return (
         <li className="imgToText">
             <div className="ques">
+                <div className="quesSentence">
+                    Type the correct answer for the sign
+                </div>
+                <ul className="quesImgList">
+                    {question.map(ques => <li className="quesImgItem">
+                            <img className="quesImg" src={ques} alt={ques} />
+                        </li>)}
+                </ul>
+                <div></div>
+            </div>
+        </li>
+    );
+};
+
+const TextToImg = (props) => {
+    const { quizType, question, answer } = props; 
+    // quizType: "image to text"
+
+    return (
+        <li className="textToImg">
+            <div className="ques">
+                <div className="quesSentence">
+                    Choose the correct answer for the text
+                </div>
                 <ul className="quesImgList">
                     {question.map(ques => <li className="quesImgItem">
                             <img className="quesImg" src={ques} alt={ques} />
@@ -22,3 +41,5 @@ export const ImgToText = (props) => {
         </li>
     );
 };
+
+export default ImgToText;
--- client/src/components/ui/Test/index.js
+++ client/src/components/ui/Test/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { useState, useEffect } from "react";
-
+import ImgToText from "../QuesTest/index";
 import "./Test.css";
 
 import axios from "axios";
